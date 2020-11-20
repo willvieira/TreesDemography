dc5e7fc28a12a7e9911772b353d359ad62e6349c
Willian <willian.vieira@usherbrooke.ca>
2020-11-20 11:22:08 -0500

    📈 competition script resef

---
+++ R/RESEF/03_competition.R
@@ -0,0 +1,193 @@
+##############################
+# Calculate competition
+# Will Vieira
+# Nov 18, 2020
+##############################
+
+
+
+##############################
+# Steps
+# - Transform subplot xy to a continuous xy at the plot level
+# - Calculate individual height using Purves et al. 2008 allometries
+# - Calculate a competition index based on neighbor trees
+##############################
+
+
+
+
+
+
+##########################################################################################
+# Transform subplot xy to a continuous xy at the plot level
+##########################################################################################
+
+# subplots are numbered with two character numbers from 0 to 9, the first represents x and the second y
+# So a subplot numbered `03` means that it is placed in the first row (0) and third (3) column of the plot
+
+# first fix the subpots under 10 that have only one character (e.g. fix `3` to `03`)
+tree_data[, subplot_id := sprintf('%02d', subplot_id)]
+reg_data[, subplot_id := sprintf('%02d', subplot_id)]
+
+
+# Fix tree_id with `NA` position
+cat(round(tree_data[is.na(x), length(unique(tree_id))]/tree_data[, length(tree_id)] * 100, 2),
+    '% of tree_id have not xy position;',
+    'these trees have a mean dbh of', round(tree_data[is.na(x), mean(dbh)/10], 2), 'cm')
+
+# get tree_id of NA position to note in obs
+missingXY <- tree_data[is.na(x), unique(tree_id)]
+tree_data[tree_id %in% missingXY, obs := paste0(obs, '; random xy position assigned by Will on nov 18, 2020')]
+
+# get random positions at the subplot level for these trees
+tree_data[is.na(x), x := sample(1:1000, 1), by = tree_id]
+tree_data[is.na(y), y := sample(1:1000, 1), by = tree_id]
+
+
+# Calculate cumulative position with te following formula
+# xy + (1000 + subplot_id[ij]) where 1000 xy is either x or y at the subplot,
+# 1000 is the size of subplot in cm, and i or j are the first or second character 
+
+# Get first or second character of subplot_id with substring()
+tree_data[, cumX := x + (1000 * as.numeric(substring(subplot_id, 1, 1)))]
+tree_data[, cumY := y + (1000 * as.numeric(substring(subplot_id, 2, 2)))]
+
+
+
+
+##########################################################################################
+# Plot trees distribution within a plot
+##########################################################################################
+
+ # angles for drawing points around the circle
+theta = seq(0, 2 * pi, length.out = 200)
+
+dir.create('plots')
+count <- 1; totalCount <- nrow(tree_data[, .N, by = .(plot_id, year)])
+for(Plot in tree_data[, unique(plot_id)])
+{
+    plotyears <- tree_data[plot_id == Plot]
+    # define max of stems for the plot
+    nbStems_max <- reg_data[plot_id == Plot, sum(nbStems), by = .(subplot_id, year)][, max(V1)]
+
+    # define limit of plot (either 5000x5000 or 1000x5000)
+    xLim <- c(0, (max(as.numeric(plotyears[, substring(subplot_id, 1, 1)])) + 1) * 1000)
+    yLim <- c(0, (max(as.numeric(plotyears[, substring(subplot_id, 2, 2)])) + 1) * 1000)
+
+    # define size of figure based on size of plot
+    figHeight = ifelse(yLim[2] == 5000, 6.72, 12.8)
+    figWidth = ifelse(xLim[2] == 5000, 6.72, 12.8)
+
+    for(yr in plotyears[, unique(year)])
+    {
+        png(filename = paste0('plots/plot', Plot, '_', yr, '.png'), height = figHeight, width = figWidth, units = 'in', res = 300)
+        toplot <- plotyears[year == yr]
+        toplot[, rowpos := .I]
+        toplot[, colState := ifelse(state2 == 'alive', '#0F6F18', '#DF0A0A')]
+
+        par(mar = rep(0, 4), oma = rep(0, 4))
+        plot(xLim, yLim, type = 'n',axes = FALSE, ann = FALSE, asp = 1)
+
+        # Grid
+        segments(seq(1000, xLim[2] - 1000, 1000), 0, seq(1000, xLim[2] - 1000, 1000), yLim[2], col = 'gray', lwd = 0.5)
+        segments(0, seq(1000, yLim[2] - 1000, 1000), xLim[2], seq(1000, yLim[2] - 1000, 1000), col = 'gray', lwd = 0.5)
+        segments(0, 0, xLim[2], 0)
+        segments(0, 0, 0, yLim[2])
+        segments(xLim[2], 0, xLim[2], yLim[2])
+        segments(0, yLim[2], xLim[2], yLim[2])
+
+        # nb stems and reg cover
+        for(subplot in reg_data[plot_id == Plot, unique(subplot_id)])
+        {
+            # nb stems
+            nbstems <- reg_data[plot_id == Plot & subplot_id == subplot & year == yr, sum(nbStems)/nbStems_max * 1000]
+
+            # rec position
+            xleft <- as.numeric(substring(subplot, 1, 1)) * 1000
+            xright <- xleft + nbstems
+            ybottom <- as.numeric(substring(subplot, 2, 2)) * 1000 + 10
+            ytop <- ybottom + 15
+            rect(xleft, ybottom, xright, ytop, border = NA, col = '#9D7660')
+
+            # reg cover
+            regcov <- reg_data[plot_id == Plot & subplot_id == subplot & year == yr, sum(regCover)]
+            
+            # rec position
+            xright <- xleft + regcov
+            ybottom <- ytop + 5
+            ytop <- ybottom + 15
+            rect(xleft, ybottom, xright, ytop, border = NA, col = '#f25700')
+        }
+
+        # legend
+        segments(0, -20, 0, -100, lwd = 0.4); segments(1000, -20, 1000, -100, lwd = 0.4); segments(0, -60, 1000, -60, lwd = 0.4)
+        text(0, -160, '0', cex = 0.6, col = '#f25700'); text(1000, -160, '100%', cex = 0.6, col = '#f25700'); text(500, -150, '% reg cover', cex = 0.7, col = '#f25700')
+        text(0, -260, '0', cex = 0.6, col = '#9D7660'); text(1000, -260, nbStems_max, cex = 0.6, col = '#9D7660'); text(500, -250, '# max of stems', cex = 0.7, col = '#9D7660')
+        text(200, yLim[2] + 80, yr)
+
+        # add trees
+        toplot[, polygon(x = dbh/10/2 * cos(theta) + cumX, y = dbh/10/2 * sin(theta) + cumY, border = colState, col = colState), by = rowpos]
+
+        dev.off()
+
+        # progress
+        cat('   Ploting ', round(count/totalCount * 100, 2), '%\r'); count <- count + 1
+    }
+}
+
+# Convert png in gif
+for(Plot in tree_data[, unique(plot_id)])
+{
+    # convert in gif
+    system(paste0('convert -delay 100 -loop 1 plots/plot', Plot, '* plots/plot_', Plot, '.gif'))
+    # delete png files
+    system(paste0('rm plots/plot', Plot, '*'))
+
+    cat('   Converting', which(Plot == tree_data[, unique(plot_id)]), 'of', tree_data[, length(unique(plot_id))], '\r')
+}
+
+
+
+
+
+
+
+##########################################################################################
+# Calculate individual height using Purves et al. 2008 allometries
+##########################################################################################
+
+
+# Calculate individual height
+
+  # First remove all species without sp_code2 (all of them also do not have DHP)
+  final_dt <- final_dt[!is.na(sp_code2)]
+  
+  # source parameters and functions from Amaël's GitHub repo
+  devtools::source_url('https://raw.githubusercontent.com/amael-ls/code_R0niche/master/createData/parametersAllometries.R')
+  devtools::source_url('https://raw.githubusercontent.com/amael-ls/code_R0niche/master/toolFunctions.R')
+
+  # get parametrised species
+  tmp <- tempfile()
+  download.file('https://github.com/amael-ls/code_R0niche/raw/master/createData/ls_speciesParametrised.rds', tmp)
+  parametrisedSpecies <- readRDS(tmp)
+  parametrisedSpecies <- sps_code$spCode[sps_code$CODE %in% parametrisedSpecies[, 1]]
+
+  # species not parametrised
+  final_dt[, unique(sp_code)][!final_dt[, unique(sp_code)] %in% parametrisedSpecies]
+
+  ## Calculate height
+  final_dt[sp_code2 %in% parametrisedSpecies, height := dbhToHeight(DHP, purves2007_allometries[species == sps_code$CODE[which(sps_code$spCode == sp_code2)], a],
+	  	purves2007_allometries[species == sps_code$CODE[which(sps_code$spCode == sp_code2)], b], mm = TRUE), by = sp_code2]
+
+
+  final_dt[is.na(height), .N]/nrow(final_dt) * 100
+  # 12% of data does not have height (NA)
+  # But most of this is due dead trees, which ecologicaly do not compete for light
+  final_dt[!is.na(DHP) & is.na(height), .N]/final_dt[!is.na(DHP), .N] * 100
+  # less than 1% of these NA for height is due to lack of species specific parameters
+  # So for those species I get the heigth based in their DHP according to a regression line for the correlation between DHP and heigth
+  # For those individuals already dead, I will keep height = 0 as they should not compete (for the S* calculation)
+  srg <- summary(lm(height ~ poly(DHP, 2 , raw = TRUE), final_dt))
+  final_dt[!is.na(DHP) & is.na(height), height := (DHP^2 * rnorm(1, srg$coefficients[3, 1], srg$coefficients[3, 2])) + (DHP * rnorm(1, srg$coefficients[2, 1], srg$coefficients[2, 2])) + rnorm(1, srg$coefficients[1, 1], srg$coefficients[1, 2])]
+
+#
