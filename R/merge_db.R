b1ce63dbd68be2931564871bc92642484953851c
Willian <willian.vieira@usherbrooke.ca>
2022-10-28 15:39:24 -0400

    add tree data before transition

--- R/merge_db.R
+++ R/merge_db.R
@@ -22,8 +22,14 @@
 
 library(data.table)
 
-growth_fia <- readRDS(file.path('data', 'FIA', 'growth_dt.RDS'))
-growth_quebec <- readRDS(file.path('data', 'quebec', 'growth_dt.RDS'))
+# Non transition db
+source(file.path('R', 'FIA', '05_cleanData.R'))
+treeData_fia <- treeData; rm(treeData)
+treeData_quebec <- readRDS(file.path('data', 'quebec', 'treeDataQuebec_beforeTransition.RDS'))
+
+# Transition db
+growth_fia <- readRDS(file.path('data', 'FIA', 'growth_transition_dt.RDS'))
+growth_quebec <- readRDS(file.path('data', 'quebec', 'growth_transition_dt.RDS'))
 
 mort_fia <- readRDS(file.path('data', 'FIA', 'mort_dt.RDS'))
 mort_quebec <- readRDS(file.path('data', 'quebec', 'mort_dt.RDS'))
@@ -37,19 +43,26 @@ fec_quebec <- readRDS(file.path('data','quebec', 'fec_dt.RDS'))
 #------------------------------------------------------
 
 # Rename plot column in quebec
+treeData_quebec[, plot_id := ID_PE]
 growth_quebec[, plot_id := ID_PE]
 mort_quebec[, plot_id := ID_PE]
 fec_quebec[, plot_id := ID_PE]
 
 # plot size
+treeData_quebec[, plot_size := 399.7312]
 growth_quebec[, plot_size := 399.7312]
 mort_quebec[, plot_size := 399.7312]
 fec_quebec[, plot_size := 399.7312]
 
+treeData_fia[, plot_size := subPlot_size]
 growth_fia[, plot_size := subPlot_size]
 mort_fia[, plot_size := subPlot_size]
 
 # Plot geo location
+plot_xy <- treeData_quebec[, sf::st_coordinates(sf::st_transform(SHAPE, 4326))]
+treeData_quebec$longitude <- plot_xy[, 'X']
+treeData_quebec$latitude <- plot_xy[, 'Y']
+
 plot_xy <- growth_quebec[, sf::st_coordinates(sf::st_transform(SHAPE, 4326))]
 growth_quebec$longitude <- plot_xy[, 'X']
 growth_quebec$latitude <- plot_xy[, 'Y']
@@ -59,7 +72,7 @@ mort_quebec$longitude <- plot_xy[, 'X']
 mort_quebec$latitude <- plot_xy[, 'Y']
 
 fec_quebec[
-    growth_quebec,
+    treeData_quebec,
     `:=`(
         'longitude' = i.longitude,
         'latitude' = i.latitude
@@ -67,26 +80,8 @@ fec_quebec[
     on = 'plot_id'
 ]
 
-# for the remaining NA, extract lat and lon from SHAPE (slow)
-get_xy <- function(SHAPE, coord = 1) {
-    sf::st_coordinates(
-        sf::st_transform(
-            sf::st_sfc(
-                sf::st_point(
-                    unlist(SHAPE)
-                ),
-                crs = growth_quebec[1, sf::st_crs(SHAPE)]
-            ),
-            4326
-        )
-    )[coord]
-}
-
-fec_quebec[is.na(latitude), latitude := sapply(SHAPE, get_xy, coord = 1)]
-fec_quebec[is.na(longitude), longitude := sapply(SHAPE, get_xy, coord = 2)]
- 
-
 # Climate variables and cell ID
+treeData_quebec[, climate_cellID := cellID]
 growth_quebec[, climate_cellID := cellID]
 mort_quebec[, climate_cellID := cellID]
 fec_quebec[, climate_cellID := cellID]
@@ -97,9 +92,9 @@ fec_quebec[, climate_cellID := cellID]
 
 # Keep same species code id
 sp_ref <- data.table()
-for(i in growth_quebec[, unique(sp_code2)])
+for(i in treeData_quebec[, unique(sp_code2)])
 {
-    sp_close <- grep(i, growth_fia[, unique(species_id)], value = TRUE)
+    sp_close <- grep(i, treeData_fia[, unique(species_id)], value = TRUE)
     
     if(length(sp_close) == 1) {
         DF <- data.frame(
@@ -124,25 +119,32 @@ sp_ref <- rbind(
     )
 )
 
+treeData_quebec[, species_id := sp_ref$sp_us[match(sp_code2, sp_ref$sp_qc)]]
 growth_quebec[, species_id := sp_ref$sp_us[match(sp_code2, sp_ref$sp_qc)]]
 mort_quebec[, species_id := sp_ref$sp_us[match(sp_code2, sp_ref$sp_qc)]]
 fec_quebec[, species_id := sp_ref$sp_us[match(sp_code2, sp_ref$sp_qc)]]
 
 # Use estimated height when no measure of height is available
+treeData_fia[, height := actual_height]
 growth_fia[, height := actual_height]
 mort_fia[, height := actual_height]
 
+treeData_fia[is.na(height), height := total_height]
 growth_fia[is.na(height), height := total_height]
 mort_fia[is.na(height), height := total_height]
 
+treeData_fia[is.na(height), height := est_height]
 growth_fia[is.na(height), height := est_height]
 mort_fia[is.na(height), height := est_height]
 
 # Delta size for quebec
+treeData_quebec[, dbh := DHP]
 growth_quebec[, deltaDbh := dbh1 - dbh0]
 mort_quebec[, deltaDbh := dbh1 - dbh0]
 
-# status
+# status (remove harvested for quebec)
+treeData_quebec <- treeData_quebec[state != 'harvested']
+treeData_quebec[, status := ifelse(state == 'alive', 1, 2)]
 growth_quebec[, status := ifelse(state == 'alive', 1, 2)]
 
 
@@ -168,13 +170,27 @@ fec_fia[, plotSize_sapling := 13.5]
 #------------------------------------------------------
 
 # add source unique ID
+treeData_fia[, db_origin := 'fia']
 growth_fia[, db_origin := 'fia']
 mort_fia[, db_origin := 'fia']
 fec_fia[, db_origin := 'fia']
 
+treeData_quebec[, db_origin := 'qc']
 growth_quebec[, db_origin := 'qc']
 mort_quebec[, db_origin := 'qc']
 fec_quebec[, db_origin := 'qc']
+
+# Name of columns to keep
+colsToKeep_treeData <- c(
+    'plot_id', 'longitude', 'latitude', 'plot_size', 'BA_plot', 'BA_comp', 'relativeBA_comp', 's_star', 'db_origin', 'bio_01_mean', 'bio_12_mean', 'bio_01_sd', 'bio_12_sd', 'climate_cellID', 'tree_id', 'species_id', 'status', 'year_measured', 'dbh', 'height', 'canopyDistance', 'BA_sp', 'relativeBA_sp'
+)
+
+# Merge treeData
+treeData <- rbind(
+    treeData_fia[, colsToKeep_treeData, with = FALSE],
+    treeData_quebec[, colsToKeep_treeData, with = FALSE]
+)
+
 
 # Name of columns to keep
 colsToKeep_growth <- c(
@@ -213,15 +229,17 @@ fec <- rbind(
 # Remove NAs
 #------------------------------------------------------
 
-growth <- growth[!(is.na(species_id) | is.na(BA_plot) | is.na(BA_sp) | is.na(s_star))]
-mort <- mort[!(is.na(species_id) | is.na(BA_plot) | is.na(BA_sp) | is.na(s_star))]
-fec <-  fec[!(is.na(species_id) | is.na(BA_plot) | is.na(s_star))]
+treeData <- treeData[!is.na(species_id)]
+growth <- growth[!is.na(species_id)]
+mort <- mort[!is.na(species_id)]
+fec <-  fec[!is.na(species_id)]
 
 
 
 # Save
 #------------------------------------------------------
 
+saveRDS(treeData, file.path('data', 'treeData.RDS'))
 saveRDS(growth, file.path('data', 'growth_dt.RDS'))
 saveRDS(mort, file.path('data', 'mort_dt.RDS'))
 saveRDS(fec, file.path('data', 'fec_dt.RDS'))
