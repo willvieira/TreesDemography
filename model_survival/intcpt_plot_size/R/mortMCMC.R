70618d5a4d978cf6a52e4d9e0a6034ddf9720c25
Willian <willian.vieira@usherbrooke.ca>
2023-10-07 16:52:10 -0400

    add surv int + plot + size

---
+++ model_survival/intcpt_plot_size/R/mortMCMC.R
@@ -0,0 +1,78 @@
+## Script to run recruitment bayesian model
+
+library(data.table)
+library(cmdstanr)
+library(tidyverse)
+
+set.seed(0.0)
+
+
+
+## Get simulation info
+
+# array (species)
+array_id <- as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+
+# load info file
+sim_info <- yaml::read_yaml('_simulation_info.yml')
+
+# species_ids
+sp <- sim_info$spIds[array_id]
+
+# load database
+dataSource <- readRDS(
+  sim_info$dataSources[
+    which(sim_info$vitalRates == 'mort')
+  ]
+)
+
+
+# select the species
+dataSource <- dataSource[species_id == sp]
+
+# remove observations with dbh lower than 127 mm
+dataSource <- dataSource[dbh0 >= 127]
+
+## define plot_id in sequence to be used in stan
+plot_id_uq <- dataSource[, unique(plot_id)]
+toSub <- data.table(
+  plot_id = plot_id_uq,
+  plot_id_seq = 1:length(plot_id_uq)
+)
+
+# merge
+dataSource[
+  toSub,
+  plot_id_seq := i.plot_id_seq,
+  on = 'plot_id'
+]
+
+## run the model
+
+stanModel <- cmdstan_model('stan/mort.stan')
+dir.create(file.path('output', sp))
+
+# Run
+md_out <- stanModel$sample(
+    data = list(
+        N = dataSource[, .N],
+        Np = dataSource[, length(unique(plot_id))],
+        plot_id = dataSource[, plot_id_seq],
+        state_t1 = dataSource[, mort],
+        delta_time = dataSource[, deltaYear],
+        size_t0 = dataSource[, dbh0]
+    ),
+    parallel_chains = sim_info$nC,
+    iter_warmup = sim_info$maxIter/2,
+    iter_sampling = sim_info$maxIter/2,
+    output_dir = file.path('output', sp)
+)
+
+## save plot_id sequence
+saveRDS(
+  toSub,
+    file = file.path(
+    'output',
+    paste0('toSub_', sp, '.RDS')
+  )
+)
